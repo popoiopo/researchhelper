cafb9557f4f03220888eb6dddc0514e9e4e50f4b
Bas Chatel <bastiaan.chatel@gmail.com>
2021-08-22 16:48:15 +0200

    Try to fix pypi.

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Setup the package."""
+
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    author='Bas Chatel',
+    author_email='bastiaan.chatel@gmail.com',
+    description='A repository of useful functions supporting research.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',  # This is important!
+    name='researchhelper',
+    url='http://github.com/popoiopo/researchhelper',
+    version='0.1.4',
+)
