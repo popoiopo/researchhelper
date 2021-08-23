e1ae68a8ba2730887450c329720d3c9848ff1967
Bas Chatel <bastiaan.chatel@gmail.com>
2021-08-23 09:37:24 +0200

    Clean start in docs folder.
    github didn't recognize the capitalization changes.

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@
 
 from setuptools import setup
 
-with open('README.rst') as f:
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
+    name='researchhelper',
     author='Bas Chatel',
     author_email='bastiaan.chatel@gmail.com',
     description='A repository of useful functions supporting research.',
     long_description=long_description,
-    long_description_content_type='text/x-rst',  # This is important!
-    name='researchhelper',
+    long_description_content_type='text/x-rst',
     url='http://github.com/popoiopo/researchhelper',
-    version='0.1.6',
+    version='0.1.7',
 )
